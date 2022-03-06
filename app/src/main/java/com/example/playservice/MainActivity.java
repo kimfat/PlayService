7d77ee91ac307f89a66b05565bdd60110225b2cd
Admin <wqf00@nk.ru>
2022-03-06 13:07:41 +0300

    .

---
+++ app/src/main/java/com/example/playservice/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.playservice;
+
+import static android.app.Service.START_FLAG_RETRY;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button btnStart = findViewById(R.id.button_start);
+        final Button btnStop = findViewById(R.id.button_stop);
+
+        // запуск службы
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // используем явный вызов службы
+                startService(
+                        new Intent(MainActivity.this, PlayService.class));
+            }
+        });
+
+        // остановка службы
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(
+                        new Intent(MainActivity.this, PlayService.class));
+            }
+        });
+    }
+}
